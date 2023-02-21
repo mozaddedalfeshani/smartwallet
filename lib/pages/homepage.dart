cac8807fc3777ad9ad27e3e1f31db8203af1ea8e
mozaddedalfeshani <mozaddedalfeshanimurad@gmail.com>
2023-02-21 16:29:52 +0600

    adding SCV

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -9,11 +9,13 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  //cpp function
   String emojiresponse() {
     int balance = BalanceControler.instance.balance;
-    if (balance < 100) {
+    int day = BalanceControler.instance.perdayneed;
+    if ((balance / day) < 2) {
       return "assets/sad.png";
-    } else if (balance > 100 && balance < 500) {
+    } else if ((balance / day) > 2 && (balance / day) < 7) {
       return "assets/twoDay.jpeg";
     } else {
       return "assets/happy.png";
@@ -40,87 +42,89 @@ class _HomePageState extends State<HomePage> {
         shadowColor: Colors.white,
       ),
       body: SafeArea(
-        child: Form(
-          key: _formkey,
-          child: Column(
-            children: [
-              Center(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    //balance show
-                    AnimatedBuilder(
-                        //
-                        //stream: null,
-                        animation: BalanceControler.instance,
-                        builder: (context, snapshot) {
-                          return Text(
-                            BalanceControler.instance.balance.toString(),
-                            style: const TextStyle(fontSize: 60),
-                          );
-                        }),
-                    const SizedBox(
-                      width: 40,
-                    ),
-                    //emoji
-                    Container(
-                      height: 50,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(25),
-                      ),
-                      child: AnimatedBuilder(
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formkey,
+            child: Column(
+              children: [
+                Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      //balance show
+                      AnimatedBuilder(
+                          //
                           //stream: null,
                           animation: BalanceControler.instance,
                           builder: (context, snapshot) {
-                            return Image.asset(
-                              emojiresponse(),
+                            return Text(
+                              BalanceControler.instance.balance.toString(),
+                              style: const TextStyle(fontSize: 60),
                             );
                           }),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              AnimatedBuilder(
-                  animation: BalanceControler.instance,
-                  builder: (context, snapshot) {
-                    return Text(
-                        "${BalanceControler.instance.dayleft()} day left!");
-                  }),
-              const SizedBox(
-                height: 20,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 100),
-                child: TextFormField(
-                  controller: wastecontroler,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(23),
-                    ),
-                    hintText: "Waste Money",
+                      const SizedBox(
+                        width: 40,
+                      ),
+                      //emoji
+                      Container(
+                        height: 50,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(25),
+                        ),
+                        child: AnimatedBuilder(
+                            //stream: null,
+                            animation: BalanceControler.instance,
+                            builder: (context, snapshot) {
+                              return Image.asset(
+                                emojiresponse(),
+                              );
+                            }),
+                      ),
+                    ],
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              //cutting button
-              //
-              //
-              ElevatedButton(
-                onPressed: () {
-                  BalanceControler.instance
-                      .waste(int.parse(wastecontroler.text));
-                  // clean the waste text field
-                  wastecontroler.clear();
-                },
-                child: const Text("Update! "),
-              ),
-            ],
+                const SizedBox(
+                  height: 20,
+                ),
+                AnimatedBuilder(
+                    animation: BalanceControler.instance,
+                    builder: (context, snapshot) {
+                      return Text(
+                          "${BalanceControler.instance.dayleft()} day left!");
+                    }),
+                const SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 100),
+                  child: TextFormField(
+                    controller: wastecontroler,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(23),
+                      ),
+                      hintText: "Waste Money",
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                //cutting button
+                //
+                //
+                ElevatedButton(
+                  onPressed: () {
+                    BalanceControler.instance
+                        .waste(int.parse(wastecontroler.text));
+                    // clean the waste text field
+                    wastecontroler.clear();
+                  },
+                  child: const Text("Update! "),
+                ),
+              ],
+            ),
           ),
         ),
       ),
