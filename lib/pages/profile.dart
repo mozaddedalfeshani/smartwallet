6427b9015ce6d90f77f0c6b759caae7e3934b1e8
mozaddedalfeshani <mozaddedalfeshanimurad@gmail.com>
2023-02-26 15:39:12 +0600

    adding some ..

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -6,17 +6,74 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            leading: Text(
-              "P R O F I L E",
-              style: GoogleFonts.lato(),
+    return Column(children: [
+      Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Container(
+          height: 200,
+          width: 200,
+          decoration: const BoxDecoration(
+            shape: BoxShape.circle,
+            color: Colors.green,
+          ),
+        ),
+      ),
+      const SizedBox(
+        height: 20,
+      ),
+      const Text("M  A  M U R A D"),
+      const SizedBox(
+        height: 20,
+      ),
+      //life time box
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: Colors.green.shade100,
+              ),
+              height: 200,
+              width: 200,
+              child: Column(children: [
+                Text(
+                  "Lifetime Use ",
+                  style: GoogleFonts.lato(),
+                ),
+              ]),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: Colors.red.shade100,
+              ),
+              height: 200,
+              width: 200,
+              child: Column(
+                children: const [
+                  Text(
+                    "All Time lost ",
+                    style: TextStyle(fontSize: 30),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
       ),
-    );
+      const SizedBox(
+        height: 40,
+      ),
+      ElevatedButton(
+        onPressed: () {},
+        child: const Text("Donate ! :) "),
+      ),
+    ]);
   }
 }
