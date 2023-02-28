6e3f4c66c1fdcbc1cfa4473666e6488b595a3bc4
saminsohag <sssohag172@gmail.com>
2023-02-28 15:25:51 +0600

    settings improved

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:smartwallet/database/database.dart';
 import 'package:smartwallet/pages/balance_controller.dart';
 import 'package:smartwallet/pages/my_app.dart';
+import 'package:smartwallet/setting/setting_controller.dart';
 
 void main() async {
   //flutter initialize
   WidgetsFlutterBinding.ensureInitialized();
+  await SettingsController.instance.loadSettings();
   await WalletDb.instance.init("wallet");
   await BalanceController.instance.loadBalance();
   runApp(const MyApp());
